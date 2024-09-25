948602b06eb244760d911cfd217e7e43eb4db3e5
anreetka <anreetka@sheridancollege.ca>
2024-09-25 01:43:47 -0400

    adding code for editing existing skill (image processing from frontend to backend and vice versa is still pending for another issue)

--- src/features/skills/addSkills.jsx
+++ src/features/skills/addSkills.jsx
@@ -1,18 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddSkillForm from "./addSkillForm";
+import ViewSkills from "./viewSkills";
+import { API_URL } from "../../constants";
+import styles from "./style.module.css";
 
 export default function AddSkills() {
   const [showForm, setShowForm] = useState(false);
+  const [skills, setSkills] = useState([]);
+  const [error, setError] = useState(false);
+  const [skillToEdit, setSkillToEdit] = useState(null);
+
+  const fetchSkills = async () => {
+    try {
+      const response = await fetch(`${API_URL}/resume/skill`, {
+        method: "GET",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setSkills(data);
+      } else {
+        setError(true);
+      }
+    } catch (error) {
+      console.error("Error fetching skills:", error);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchSkills();
+  }, []);
+
+  const addSkill = async (newSkill) => {
+    await fetchSkills();
+    setShowForm(false); 
+  };
+
+  const updateSkill = async (updatedSkill) => {
+    await fetchSkills();
+    setShowForm(false);
+    setSkillToEdit(null);
+  };
+
+  const handleEditSkill = (skill, index) => {
+    setSkillToEdit({ ...skill, index });
+    setShowForm(true);
+  };
+
   return (
     <section>
       {showForm ? (
-        <AddSkillForm setShowForm={setShowForm} />
+        <AddSkillForm
+          setShowForm={setShowForm}
+          addSkill={addSkill}
+          skillToEdit={skillToEdit}
+          onUpdate={updateSkill}
+        />
       ) : (
         <>
-          <p>You can add your skills in here</p>
-          <button onClick={() => setShowForm(true)}>Add Skill</button>
+          {error ? (
+            <p>Error fetching skills. Please refresh.</p>
+          ) : (
+            <ViewSkills skills={skills} onEditSkills={handleEditSkill} />
+          )}
+          <div className={styles.btncontainer} >
+            <button
+              onClick={() => {
+                setSkillToEdit(null);
+                setShowForm(true);
+              }}
+            >
+              Add Skill
+            </button>
+          </div>
         </>
       )}
     </section>
   );
-}
+}  
